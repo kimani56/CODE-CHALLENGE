b2f37c611a163e93b1a76c6399654f69e730b6bf
gift kimani <gift.kimani@students.moringaschool.com>
2023-06-19 00:02:26 +0300

    first commit

---
+++ challenge 1.js
@@ -0,0 +1,27 @@
+function calculateGrade() {
+    let marks = prompt("Enter student marks (between 0 and 100):");
+    marks = parseInt(marks);
+  
+    if (isNaN(marks) || marks < 0 || marks > 100) {
+      console.log("Invalid input. Please enter a valid mark between 0 and 100.");
+      return;
+    }
+  
+    let grade;
+  
+    if (marks > 79) {
+      grade = "A";
+    } else if (marks >= 60 && marks <= 79) {
+      grade = "B";
+    } else if (marks >= 50 && marks <= 59) {
+      grade = "C";
+    } else if (marks >= 40 && marks <= 49) {
+      grade = "D";
+    } else {
+      grade = "E";
+    }
+  
+    console.log(`Grade: ${grade}`);
+  }
+
+
---
+++ challenge 2.js
@@ -0,0 +1,19 @@
+// Prompt the user for input
+var speed = parseInt(prompt("Enter the car's speed (in km/h):"));
+
+// Define the speed limit and the demerit points increment
+var speedLimit = 70;
+var demeritPointsIncrement = 5;
+
+// Calculate the demerit points
+var demeritPoints = 0;
+if (speed >= speedLimit) {
+  demeritPoints = Math.floor((speed - speedLimit) / demeritPointsIncrement);
+}
+
+// Output the result
+if (demeritPoints === 0) {
+  console.log("Ok");
+} else {
+  console.log("Demerit points:", demeritPoints);
+}
